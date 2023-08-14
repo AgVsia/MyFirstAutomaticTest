5c42d56ec096da802cd432ea2cbe270f3eff735f
AgVsia <agnieszkazet9@wp.pl>
2023-08-14 11:04:43 +0200

    AssertPractice ( pytanie dlaczego test na assert false sie nie wywala skoro jest prawdą?

--- src/test/java/AssertsPracticeTests.java
+++ src/test/java/AssertsPracticeTests.java
@@ -6,15 +6,18 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class AssertsPracticeTests {
     private WebDriver driver;
+
 
     @BeforeTest
     public void beforeTest() {
         driver = new ChromeDriver();
         driver.navigate().to("https://petstore.octoperf.com/actions/Catalog.action");
     }
+
     @Test
     public void checkIfFishOnLeftSideIsDisplayed() {
 
@@ -26,40 +29,55 @@ public class AssertsPracticeTests {
 
     @Test
     public void checkIfDogsOnLeftSideIsDisplayed() {
+        assertTrue(driver.findElement(By.xpath("//img[@src='../images/dogs_icon.gif']")).isDisplayed());
+        assertFalse(!driver.findElement(By.xpath("//img[@src='../images/dogs_icon.gif']")).isDisplayed());
     }
 
     @Test
     public void checkIfCatsOnLeftSideIsDisplayed() {
+        assertTrue(driver.findElement(By.xpath("//img[@src='../images/cats_icon.gif']")).isDisplayed());
+        assertFalse(!driver.findElement(By.xpath("//img[@src='../images/cats_icon.gif']")).isDisplayed());
 
     }
 
     @Test
     public void checkIfReptilesOnLeftSideIsDisplayed() {
+        assertTrue(driver.findElement(By.xpath("//img[@src='../images/reptiles_icon.gif']")).isDisplayed());
+        assertFalse(!driver.findElement(By.xpath("//img[@src='../images/reptiles_icon.gif']")).isDisplayed());
+
 
     }
 
     @Test
     public void checkIfSearchContentBarIsDisplayed() {
+        assertTrue(driver.findElement(By.xpath("//*[@id='SearchContent']/form")).isDisplayed());
+        assertFalse(!driver.findElement(By.xpath("//*[@id='SearchContent']/form")).isDisplayed());
+
 
     }
 
     @Test
     public void checkIfMainBirdImageIsDisplayed() {
 
+        assertTrue(driver.findElement(By.xpath("//*[@id='MainImageContent']/map/area[1]")).isDisplayed());
+        assertFalse(!driver.findElement(By.xpath("//*[@id='MainImageContent']/map/area[1]")).isDisplayed());
+
     }
 
     @Test
     public void checkIfSearchButtonHasTypeSubmit() {
-
+        assertTrue(driver.findElement(By.xpath("//*[@id='SearchContent']/form/input[2]")).isDisplayed());
+        assertFalse(!driver.findElement(By.xpath("//*[@id='SearchContent']/form/input[2]")).isDisplayed());
     }
 
     @Test
     public void checkIfSearchContentBarHasSize14() {
-
+    assertTrue(driver.findElement(By.xpath("//*[@id='SearchContent']/form/input[1]")).isDisplayed());
+    assertFalse(!driver.findElement(By.xpath("//*[@id='SearchContent']/form/input[1]")).isDisplayed());
     }
 
     @AfterTest
-    public void afterTest(){
+    public void afterTest() {
         driver.close();
         driver.quit();
     }
